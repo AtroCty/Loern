45760542af817f203cdf900353b0e15aa0900e27
Timm Schütte <atrocty@gmx.de>
2014-03-04 15:23:43 +0100

    Aufrufen der 2. Activity

--- src/de/atrocty/loern/MainActivity.java
+++ src/de/atrocty/loern/MainActivity.java
@@ -2,21 +2,35 @@ package de.atrocty.loern;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.game);
+		Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	@Override
+	public void onClick(View view) 
+	{
+		startActivity(new Intent(this, GameActivity.class));
+	}
 }
