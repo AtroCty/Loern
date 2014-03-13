d4da17abc916098eede52a719dd7b51f6b837950
Timm Schütte <atrocty@gmx.de>
2014-03-13 14:01:47 +0100

    Updates texts

--- src/de/atrocty/loern/GameActivity.java
+++ src/de/atrocty/loern/GameActivity.java
@@ -1,5 +1,7 @@
 package de.atrocty.loern;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
@@ -9,12 +11,21 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import de.atrocty.loern.AutoResizeTextView;
 
 public class GameActivity extends Activity implements OnClickListener, Runnable
 {
 	public long fileSize = 0;
 	public int mode = 0, punkte = 0, zeit = 1200;
 	private Handler handler = new Handler();
+	private Random random = new Random();
+	
+	String[] Frage = {
+		"Was ist der Sinn des Lebens?",
+		"Wie lautet das Ohmsche Gesetz?",
+		"Ach was, war doch nur Spaﬂ",
+		"lol"
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -65,9 +76,13 @@ public class GameActivity extends Activity implements OnClickListener, Runnable
 					
 			ProgressBar progressbar1 = (ProgressBar)findViewById(R.id.progressBar1);
 			progressbar1.setVisibility(View.VISIBLE);
+			progressbar1.setProgress(zeit);
 		}	
 		TextView tvPunkte = (TextView)findViewById(R.id.punkte);
 		tvPunkte.setText(Integer.toString(punkte));
+		
+		AutoResizeTextView tvFrage = (AutoResizeTextView)findViewById(R.id.frage);
+		tvFrage.setText(getQuestion());
 	}
 	
 	public void countdown()
@@ -75,6 +90,13 @@ public class GameActivity extends Activity implements OnClickListener, Runnable
 		zeit -= 1;
 		refresh();
 		handler.postDelayed(this,50);
+	}
+	
+
+	public String getQuestion()
+	{		
+		int zufall = (int) (Frage.length * random.nextFloat());
+		return Frage[zufall];
 	}
 	
 	@Override
